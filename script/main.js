3d08435bc8bfbb9bf13c2c9efaa1938103e72868
Aqua_Lykofos <65450348+DarkSatella@users.noreply.github.com>
2024-02-06 03:41:10 +0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -7,7 +7,7 @@ const startMusic = () => {
   TweenMax.to(".one", 1, { opacity: 1, y: 0 }); // Adjust selectors and animations as needed
   TweenMax.to(".two", 1, { opacity: 1, y: 0 });
   // ... (Add more transitions for other elements)
-}
+};
 // Animation Timeline
 const animationTimeline = () => {
   // Spit chars that needs to be animated individually
